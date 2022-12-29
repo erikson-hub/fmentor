dcb722acd5077b294c8c6afd85514c9bff4205f7
erikson <erikson.velro@gmail.com>
2022-12-29 16:40:41 -0500

    updates

--- fmentor/src/app/crew/crew.module.ts
+++ fmentor/src/app/crew/crew.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { TextsComponent } from './texts/texts.component';
+import { ImageComponent } from './image/image.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TextsComponent, ImageComponent, ResultComponent],
+  imports: [CommonModule],
+  exports: [ImageComponent, TextsComponent, ResultComponent],
 })
-export class CrewModule { }
+export class CrewModule {}
