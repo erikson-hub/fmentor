8f0e1cf146e01d74d1cef8e8c07b694f5a6b5175
DLinoC <danfer200017@gmail.com>
2022-12-30 13:48:05 -0500

    Se agrego cambio la pagina de destination

--- fmentor/src/app/destination/destination.module.ts
+++ fmentor/src/app/destination/destination.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BodyComponent } from './body/body.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BodyComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    BodyComponent
   ]
 })
 export class DestinationModule { }
